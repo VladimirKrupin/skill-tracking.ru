851dbe3961ad734d6b0cddaa99a89525cf104da7
Vladimir Krupin <vladimir.krupin133@gmail.com>
2019-09-28 14:18:13 +0400

    composer update

--- Autorun/scripts-mysql/example-mysql-set-prod-access.sh
+++ Autorun/scripts-mysql/example-mysql-set-prod-access.sh
@@ -13,9 +13,10 @@ passroot='root'
 passuser='user'
 user='user'
 db='skill'
-echo "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" | mysql -uroot -p${passroot}
-echo "create user '${user}'@'172.30.0.3' identified by '${passuser}';" | mysql -uroot -p${passroot}
-echo "create user '${user}'@'172.30.0.5' identified by '${passuser}';" | mysql -uroot -p${passroot}
-echo "grant all privileges on ${db} . * to '${user}'@'172.30.0.3';" | mysql -uroot -p${passroot}
-echo "grant all privileges on ${db} . * to '${user}'@'172.30.0.5';" | mysql -uroot -p${passroot}
+echo "create database ${db};" | mysql -uroot -p${passroot}
+echo "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');" | mysql -uroot -phGS20dnlsFGfhsc3sf
+echo "create user '${user}'@'172.20.0.3' identified by '${passuser}';" | mysql -uroot -p${passroot}
+echo "create user '${user}'@'172.20.0.5' identified by '${passuser}';" | mysql -uroot -p${passroot}
+echo "grant all privileges on ${db} . * to '${user}'@'172.20.0.3';" | mysql -uroot -p${passroot}
+echo "grant all privileges on ${db} . * to '${user}'@'172.20.0.5';" | mysql -uroot -p${passroot}
 echo "FLUSH PRIVILEGES;" | mysql -uroot -p${passroot}
