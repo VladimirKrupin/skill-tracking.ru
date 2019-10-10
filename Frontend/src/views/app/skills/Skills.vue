6f2aeb1151e158bc59130c674bacab33ab73db4b
Vladimir Krupin <vladimir.krupin133@gmail.com>
2019-10-10 14:31:21 +0400

    add skill cards

--- Frontend/src/views/app/skills/Skills.vue
+++ Frontend/src/views/app/skills/Skills.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <return></return>
-      <div class="row col-12">
-        <b-card class="mr-5" :class="item.bg" v-for="(item, index) in skills" :key="index">
+      <div class="row pr-4 pl-4">
+        <b-card class="col-xl-3 col-lg-5 col-md-5 col-sm-5 mr-sm-0 mr-md-5" :class="item.bg" v-for="(item, index) in skills" :key="index">
           <router-link :to="'skills/'+item.title" class="text-muted card-link">
             <b-card-body class="pb-0">
               <b-dropdown class="float-right" variant="transparent p-0" right>
@@ -17,9 +17,10 @@
               <h4 class="mb-0 text-white">{{item.title}}</h4>
               <p class="text-white">{{item.description}}</p>
             </b-card-body>
-            <card-line1-chart-example v-if="item.type === 1" chartId="card-chart-01" class="chart-wrapper px-3 overflow-visible" style="height:115px;" :height="70"/>
-            <card-line2-chart-example v-if="item.type === 2" chartId="card-chart-02" class="chart-wrapper px-3 overflow-visible" style="height:115px;" :height="70"/>
-          </router-link>
+            </router-link>
+          <card-line1-chart-example v-if="item.type === 1" chartId="card-chart-01" class="chart-wrapper px-3 overflow-visible" style="height:115px;" :height="70"/>
+          <card-line2-chart-example v-if="item.type === 2" chartId="card-chart-02" class="chart-wrapper px-3 overflow-visible" style="height:115px;" :height="70"/>
+
         </b-card>
       </div>
     </div>
